6ee8c40a88c7e6163e45f0d06f5bf681b8191a87
hicker-coder <hicker.coder.2023@gmail.com>
2023-05-17 10:14:39 +0100

    added constants.py file for data preparation processor

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Constants for column name mapping
+NAME_MAPPING = {
+    'Employee Name': 'full_name',
+    'Company/Display Name': 'company_name',
+    'Job Position/Display Name': 'job_position',
+    'Hr Team/Display Name': 'hr_team',
+    'Work Location/Display Name': 'location',
+    'Job Position - New Nomenclature':'new_job_position',
+    'Job Family':'job_family',
+    'Image': 'image_path'
+}
+
+# Constants for HR teams
+HR_TEAMS = ["McK Team", "Office Admin team", "IK Team", "BCG ME Team", "Business Translation Team", "IT Team",
+            "Finance Team", "BCG Europe Team", "Graphic Design Team", "BCG Africa Team", "Marketing team",
+            "BCG KT support", "Operations Team", "Data Analytics Services", "Business Development Team",
+            "HR Team", "Research Team", "Executive Management", "IKT/Finance"]
+
+# Constants for job families
+JOB_FAMILIES = ['Business Research','Office Management','Business Translation','IT','Finance','Graphic Design','Marketing',
+                'Operations','Data Analytics','Business Development','Human Capital','Executive Management','Sales Representative']
